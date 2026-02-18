0092ced7de49a74dc466a79dc1216c78565218d5
jaydooroo <jehyeonlee27@gmail.com>
2026-02-17 19:47:18 -0600

    deleted default

--- backend/strategies/__init__.py
+++ backend/strategies/__init__.py
@@ -1,12 +1,10 @@
 
 from .paa_strategy import PAAStrategy
-from .simple_momentum import SimpleMomentumStrategy
 from .vaa_strategy import VAAStrategy
 
 # Registry of all available strategies
 STRATEGIES = {
     'paa': PAAStrategy(),
-    'simple_momentum': SimpleMomentumStrategy(),
     'vaa': VAAStrategy()
 }
 
